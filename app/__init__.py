f4e66b9b66dae79a6faae951c3167c7be8ddb5f5
sichoi <clogic@toss.im>
2019-10-09 17:16:24 +0900

    middleware 추가

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,23 @@
 import hug
+import json
 
 from app.models import *
+from app.db import session
+
+
+@hug.request_middleware()
+def request(request: hug.Request, response: hug.Response):
+    request.context['packet'] = Packet(request_header=request.headers, request_body=request.params)
+
+@hug.response_middleware()
+def response(request, response: hug.Request, resource: hug.Response):
+    packet = request.context['packet']
+    packet.response_header = response.headers
+    packet.response_body = json.loads(response.data)
+    session.add(packet)
+    session.commit()
 
 
 @hug.get('/')
-def index():
-    return 'hello world'
+def index() -> dict:
+    return { 'id': '123', 'text': 'test' }
