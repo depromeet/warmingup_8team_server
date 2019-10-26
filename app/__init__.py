355446c4f671a42301829387a6c4640e608d48bc
sichoi <clogic@toss.im>
2019-10-27 00:07:16 +0900

    login 연결

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@
 # isort:skip_file
 from app.config import Config
 from flask import Flask
+from flask_cors import CORS
 
 app: Flask = Flask(__name__)
 app.secret_key = Config.SECRET_KEY
 app.config.from_object(Config)
+CORS(app, supports_credentials=True)
 
 
 from app.api import *
