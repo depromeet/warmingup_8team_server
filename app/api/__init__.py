32ba0aec215702eec639439ae7064d52c27bc9d8
권재원 <springkjw@gmail.com>
2019-11-02 16:16:52 +0900

    소켓 연결한 것

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,10 +1,19 @@
+import json
 import requests
 from app import app, models
 from app.context import ApiContext
 from app.decorators import router
 from flask import session
+from flask_socketio import SocketIO, send
+from flask_cors import CORS
 
 route = router(app)
+socket_io = SocketIO(
+    app,
+    host='0.0.0.0',
+    port=5000,
+    cors_allowed_origins="*"
+)
 
 
 @route('/login', methods=['POST'])
@@ -82,3 +91,18 @@ def send_message(context: ApiContext) -> dict:
     m = context.user.send_message(context.data['text'])
 
     return m.to_json()
+
+
+@socket_io.on("message")
+def request(data):
+    message = data.get('message', '')
+    # send({}, broadcast=True)
+    # to_client = dict()
+    # if message == 'new_connect':
+    #     to_client['message'] = "새로운 유저가 난입하였다!!"
+    #     to_client['type'] = 'connect'
+    # else:
+    #     to_client['message'] = message
+    #     to_client['type'] = 'normal'
+    # # emit("response", {'data': message['data'], 'username': session['username']}, broadcast=True)
+    # send(to_client, broadcast=True)
