0be4ab21d21fb9d53e3f566b79f27a00c8bff033
sichoi <clogic@toss.im>
2019-11-03 02:39:43 +0900

    ??

---
+++ app/config/dev.py
@@ -0,0 +1,10 @@
+from app.config.base import BaseConfig
+
+
+class Config(BaseConfig):
+    SECRET_KEY = 'dfasdfsdf'
+
+    DB_HOST = 'kkirook.cy4xhqwzngh6.ap-northeast-2.rds.amazonaws.com'
+    DB_USERNAME = 'kkirook'
+    DB_PASSWORD = 'kkirook1234!'
+    DB_NAME = 'kkirook'
